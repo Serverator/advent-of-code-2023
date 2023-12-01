ceeed94664904dfaf56a49aa61961249133ebb8c
Serv <smka018@gmail.com>
2023-12-01 09:55:45 +0400

    Rewrite

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -21,53 +21,38 @@ fn main() {
 }
 
 fn part1(input: &str) {
-    let mut a = 0;
-    for line in input.lines() {
-        let mut iter = line.chars().filter(|x| x.is_numeric());
+    let result = input.lines().map(|l| {
+        let vec: Vec<_> = l.chars().filter(|c| c.is_numeric()).collect();
+        format!("{}{}", vec.first().unwrap(), vec.last().unwrap()).parse::<i32>().unwrap()
+    }).sum::<i32>();
 
-
-        let first = iter.next().unwrap();
-        let last = iter.last().unwrap_or(first);
-        let mut together = first.to_string();
-        together.push(last);
-
-        a += together.parse::<i32>().unwrap();
-    }
-
-    println!("{}", a);
+    println!("{}", result);
 }
 
 fn part2(input: &str) {
-    let mut a = 0;
-    for line in input.lines() {
-
+    let result = input.lines().map(|l| {
         let mut vec = Vec::new();
-        for i in 0..line.len() {
-            let line = &line[i..];
+        for i in 0..l.len() {
+            let l = &l[i..];
 
-           let num = if line.chars().next().map(|x| x.is_numeric()).unwrap_or_default() {
-                line.chars().next().unwrap().to_string().parse::<i32>().unwrap()
-           }
-            else if line.starts_with("one") { 1 }
-           else if line.starts_with("two") { 2 } 
-           else if line.starts_with("three") { 3 }
-           else if line.starts_with("four") { 4 }
-           else if line.starts_with("five") { 5 }
-           else if line.starts_with("six") { 6 } 
-           else if line.starts_with("seven") { 7 }
-           else if line.starts_with("eight") { 8 }
-           else if line.starts_with("nine") { 9 } 
-           else { continue };
-
-           vec.push(num)
+            let num = if l.chars().next().map(|x| x.is_numeric()).unwrap_or_default() {
+                 l.chars().next().unwrap().to_digit(10).unwrap()
+            }
+            else if l.starts_with("one") { 1 }
+            else if l.starts_with("two") { 2 } 
+            else if l.starts_with("three") { 3 }
+            else if l.starts_with("four") { 4 }
+            else if l.starts_with("five") { 5 }
+            else if l.starts_with("six") { 6 } 
+            else if l.starts_with("seven") { 7 }
+            else if l.starts_with("eight") { 8 }
+            else if l.starts_with("nine") { 9 } 
+            else { continue };
+        
+            vec.push(num)
         }
+        format!("{}{}", vec.first().unwrap(), vec.last().unwrap()).parse::<i32>().unwrap()
+    }).sum::<i32>();
 
-        let first = vec.first().unwrap();
-        let last = vec.last().unwrap();
-        let together = first.to_string() + last.to_string().as_str();
-
-        a += together.parse::<i32>().unwrap();
-    }
-    
-    println!("{}", a);
+    println!("{}", result);
 }
