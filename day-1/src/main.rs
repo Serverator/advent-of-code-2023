33281995f96f71018c5a21f42b3ca6c269320679
Serv <smka018@gmail.com>
2023-12-01 08:50:28 +0400

    Shitty template

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,20 @@
+use std::fmt::Debug;
+
 fn main() {
-    println!("Hello, world!");
+    let run_example = std::env::args().any(|arg| arg == "--ex");
+
+    let input = if run_example {
+        std::fs::read_to_string("./example.txt").unwrap()
+    } else {
+        std::fs::read_to_string("./input.txt").unwrap()
+    };
+
+    compute(&input);
+}
+
+fn compute(input: &str) -> Option<()> {
+
+    println!("{}", input);
+
+    None
 }
