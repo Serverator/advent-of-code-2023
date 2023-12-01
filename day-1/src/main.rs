5bab095a6cdd15c3238a22b02b61b637d9322160
Serv <smka018@gmail.com>
2023-12-01 14:42:50 +0400

    Improved code

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 fn main() {
     let run_example = std::env::args().any(|arg| arg == "--ex");
     let part_two = std::env::args().any(|arg| arg == "--p2");
@@ -29,27 +31,36 @@ fn part1(input: &str) {
     println!("{}", result);
 }
 
+const DIGITS: &[(&str, u32)] = &[
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
 fn part2(input: &str) {
     let result = input.lines().map(|l| {
         let mut vec = Vec::new();
         for i in 0..l.len() {
             let l = &l[i..];
 
-            let num = if l.chars().next().map(|x| x.is_numeric()).unwrap_or_default() {
-                 l.chars().next().unwrap().to_digit(10).unwrap()
+            if let Some(char) = l.chars().next() {
+                if char.is_numeric() {
+                    vec.push(char.to_digit(10).unwrap());
+                    continue;
+                }
             }
-            else if l.starts_with("one") { 1 }
-            else if l.starts_with("two") { 2 } 
-            else if l.starts_with("three") { 3 }
-            else if l.starts_with("four") { 4 }
-            else if l.starts_with("five") { 5 }
-            else if l.starts_with("six") { 6 } 
-            else if l.starts_with("seven") { 7 }
-            else if l.starts_with("eight") { 8 }
-            else if l.starts_with("nine") { 9 } 
-            else { continue };
-        
-            vec.push(num)
+
+            for (digit, num) in DIGITS {
+                if l.starts_with(digit) {
+                    vec.push(*num);
+                }
+            }
         }
         format!("{}{}", vec.first().unwrap(), vec.last().unwrap()).parse::<i32>().unwrap()
     }).sum::<i32>();
